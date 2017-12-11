d401df42f448ab7b3669ffeba35ae062e48baca1
Scott Pierce <ddrscott@gmail.com>
2017-12-10 19:16:55 -0600

    It works!

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,23 @@
-// This is where it all goes :)
+(function($){
+  jQuery(function(){
+    $('#main-text').keyup(function(e) {
+      var letters = e.target.value;
+      var tags = '';
+      for (var x = 0; x < letters.length; x++) {
+        var c = letters.charAt(x).toLowerCase();
+        if (c == '!') {
+          c = 'exclamation';
+          tags += '<img src="/images/unowns/201-exclamation.png" alt="unown-exclamation">'
+        } else if (c == '?') {
+          c = 'question';
+          tags += '<img src="/images/unowns/201-question.png" alt="unown question">'
+        } else if (c >= 'a' && c <= 'z') {
+          tags += '<img src="/images/unowns/201-' + c + '.png" alt="unown ' + c + '">'
+        } else {
+          tags += c
+        }
+      }
+      $('#unowns').html(tags);
+    })
+  })
+})(jQuery)
